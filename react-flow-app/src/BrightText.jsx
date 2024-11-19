e6e3cc647ce5cd5b59a4bd26f3083e75c9f4ab99
Sumanth Devarapalli <79638592+sumanthdevarapalli@users.noreply.github.com>
2024-11-19 12:08:25 +0530

    Delete react-flow-app directory

--- react-flow-app/src/BrightText.jsx
+++
@@ -1,9 +0,0 @@
-function BrightText({color}){
-    
-    return(
-        <>
-            <h1 style={{color:color}}>red</h1>
-        </>
-    )
-}
-export default BrightText;
--- react-flow-app/src/PostTitle.jsx
+++
@@ -1,9 +0,0 @@
-function PostTitle({title})
-{
-    return(
-        <>
-        <h1>title:{title}</h1>
-        </>
-    )
-}
-export default PostTitle;
